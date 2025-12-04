edd17f74e89dada0601636f6450e296e069fde5e
Vali <vali.moritz@gmail.com>
2025-12-04 15:59:59 +0100

    Initial Commit: Frontend + Config für Deployment in package.json und vite.congfig.ts angepasst!

---
+++ README.md
@@ -0,0 +1,6 @@
+## Code Ausführen:
+
+  Run `npm i` to install the dependencies.
+
+  Run `npm run dev` to start the development server.
+  
---
+++ src/vite-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="vite/client" />
+
+// Falls der Fehler trotzdem bleibt, füge das hier explizit hinzu:
+declare module '*.css';
